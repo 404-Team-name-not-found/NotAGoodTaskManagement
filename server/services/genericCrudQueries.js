2d8f34b2af6c072d734c4b92558de09c8f706f0a
alon manshari <alonman17@gmail.com>
2022-01-07 17:30:13 +0200

    feat(generic and specific db queries)

---
+++ server/services/genericCrudQueries.js
@@ -0,0 +1,103 @@
+const pool = require("./server/services/sql.connections").pool;
+/**
+ * Given a table name, return all the rows within the table
+ * @param {String} tableName
+ * @returns {object}
+ */
+async function getItems(tableName) {
+  const client = await pool.connect();
+  const res = client.query(`SELECT * FROM "${tableName}"`);
+  client.release();
+  return (await res).rows;
+}
+
+/**
+ * Given a table name and a specific id, return the specifc row from the table with the given id
+ * @param {string} tableName
+ * @param {number} id
+ * @returns {object}
+ */
+async function getItem(tableName, id) {
+  const client = await pool.connect();
+  const res = client.query(`SELECT * FROM "${tableName}" WHERE id=${id}`);
+  client.release();
+  return (await res).rows;
+}
+
+/**
+ * Given a table name and an id, remove the row in the table with the given id
+ * @param {String} tableName
+ * @param {Number} id
+ * @returns
+ */
+async function DeleteItem(tableName, id) {
+  const client = await pool.connect();
+  const res = client.query(
+    `DELETE FROM ${tableName} WHERE id=${id} RETURNING * `
+  );
+  client.release();
+  return (await res).rows;
+}
+
+/**
+ * check if the given value is exist in the table in the given column
+ * @param {String} tablename
+ * @param {String} colname
+ * @param {*} value
+ * @returns
+ */
+async function checkExist(tablename, colname, value) {
+  const client = await pool.connect();
+  const res = client
+    .query(`SELECT count(*) FROM  "${tablename}" where ${colname}='${value}'`)
+    .then((res) => res.rows[0].count >= 1);
+  client.release();
+  return await res;
+}
+
+//helper function
+const getKeysAndValues = (object) => {
+  delete object.id;
+  const keys = Object.keys(object);
+  return [keys, keys.map((key) => object[key])];
+};
+
+/**
+ * update a specific item with the given id in the given table
+ * @param {Number} itemId the item id
+ * @param {String} tableName the table name
+ * @param {Object} change the object AFTER the change
+ * @returns
+ */
+async function updateSpecificItem(itemId, tableName, change) {
+  [keys, values] = getKeysAndValues(change);
+  const update = keys.map((key, index) => `"${key}" = $${index + 1}`).join(",");
+  const query = `UPDATE public."${tableName}" SET ${update} WHERE id = ${itemId} RETURNING *`;
+  const client = await pool.connect();
+  const res = client.query(query, values);
+  client.release();
+  return await (
+    await res
+  ).rows;
+}
+/**
+ *
+ * @param {String} query the query
+ * @param {Array} values values in the query
+ * @returns
+ */
+async function sendCustomQuery(query, values) {
+  const client = await pool.connect();
+  const res = client.query(query, values);
+  client.release();
+  return (
+    await res
+  ).rows;
+}
+
+module.exports.DeleteItem = DeleteItem;
+module.exports.getItems = getItems;
+module.exports.getItem = getItem;
+module.exports.updateSpecificItem = updateSpecificItem;
+module.exports.checkExist = checkExist;
+module.exports.sendCustomQuery=sendCustomQuery;
---
+++ server/services/insertQueries.js
@@ -0,0 +1,74 @@
+const pool = require("./server/services/sql.connections").pool;
+
+async function insertTask(name,estimation,creationDate,status,assigedTo,creator,sprintId,storyId, groupId){
+  const client = await pool.connect();
+  const res = client.query(
+    `INSERT INTO public."Tasks"(
+         name, estimation, "creationDate", status, "assigedTo", creator, "sprintId", "storyId", "groupId")
+        VALUES ( '${name}', '${estimation}', '${creationDate}', '${status}', '${assigedTo}', '${creator}', '${sprintId}', '${storyId}', ${groupId}) returning *`
+  );
+  client.release();
+  return await res;
+}
+
+async function insertUser(username, password,imgurl,email, title, role,phone,groupid) {
+  const client = await pool.connect();
+  const res = client.query(
+    `INSERT INTO public."Users"(
+        username, password, "imgUrl", title, role, email, phone, groupid)
+        VALUES ('${username}', '${password}', '${imgurl}','${email}', '${title}', '${role}', '${phone}', ${groupid}) returning *`
+  );
+  client.release();
+  return await res.rows;
+}
+
+async function insertMessage(title, author, content, taskid) {
+  const client = await pool.connect();
+  const res = client.query(
+    `INSERT INTO public."Messages"(
+         title, author, content, "taskId")
+        VALUES ( '${title}', '${author}', '${content}', '${taskid}')`
+  );
+  client.release();
+  return await res.rows;
+}
+
+async function insertSprint(name, description, duration, startDate) {
+  const client = await pool.connect();
+  const res = client.query(
+    `INSERT INTO public."Sprints"(
+         name, description, duration, "startDate")
+        VALUES ( '${name}', '${description}', '${duration}', '${startDate}')`
+  );
+  client.release();
+  return await res.rows;
+}
+
+async function insertGroup(name, numberOfUsers, description, imgUrl) {
+  const client = await pool.connect();
+  const res = client.query(
+    `INSERT INTO public."Groups"(
+         name, "numOfUsers", description, "imgUrl")
+        VALUES ('${name}' , '${numberOfUsers}', '${description}', '${imgUrl}')`
+  );
+  client.release();
+  return await res.rows;
+}
+
+async function insertStory(name, icon, color, description, startDate, endDate) {
+  const client = await pool.connect();
+  const res = client.query(
+    `INSERT INTO public."Story"(
+         name, icon, color, description, "startDate", "endDate")
+        VALUES ( '${name}', '${icon}', '${color}', '${description}', '${startDate}', '${endDate}');`
+  );
+  client.release();
+  return await res.rows;
+}
+
+module.exports.insertGroup=insertGroup;
+module.exports.insertMessage=insertMessage;
+module.exports.insertSprint=insertSprint;
+module.exports.insertStory=insertStory;
+module.exports.insertTask=insertTask;
+module.exports.insertUser=insertUser;
