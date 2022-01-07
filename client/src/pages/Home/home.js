8b9620e35d2792a1b833b58d9648c5accf91b1b5
sapirmosko <sapirmo2000@gmail.com>
2022-01-07 18:20:53 +0200

    fix(sidebar): fix css pr
    feat(sidebar): Create new home page with sidebar

---
+++ client/src/pages/Home/home.js
@@ -0,0 +1,5 @@
+const backlog = document.getElementById("backlog");
+const sprint = document.getElementById("sprint");
+const stories = document.getElementById("stories");
+const profile = document.getElementById("profile");
+const content = document.getElementById("content");
